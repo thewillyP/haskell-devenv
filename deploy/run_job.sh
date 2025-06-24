6669ac5e0a9fcc64d50805ffc6466074805d79c6
thewillyP <williamsleepark@gmail.com>
2025-06-24 18:01:33 -0400

    now accepts extra env variables

--- deploy/run_job.sh
+++ deploy/run_job.sh
@@ -7,12 +7,22 @@ BUILD_JOB_ID=$5
 VNC_PASSWORD=$6
 IMAGE=$7
 TMP_DIR=$8
+EXTRA_ENV_VARS=$9
 
 # Dependency
 if [ -n "$BUILD_JOB_ID" ]; then
     SLURM_DEPENDENCY="#SBATCH --dependency=afterok:$BUILD_JOB_ID"
 else
     SLURM_DEPENDENCY=""
+fi
+
+# Parse EXTRA_ENV_VARS into Singularity --env arguments
+ENV_ARGS=""
+if [ -n "$EXTRA_ENV_VARS" ]; then
+    # Split comma-separated key=value pairs and convert to --env arguments
+    ENV_ARGS=$(echo "$EXTRA_ENV_VARS" | tr ',' '\n' | while IFS='=' read -r key value; do
+        echo "--env $key=$value"
+    done | tr '\n' ' ')
 fi
 
 # Submit SLURM job
@@ -31,6 +41,7 @@ ${SLURM_DEPENDENCY}
 singularity run --containall --cleanenv --no-home \\
   --overlay ${OVERLAY_PATH}:rw \\
   --env VNC_PASSWORD=${VNC_PASSWORD} \\
+  ${ENV_ARGS} \\
   --bind /home/${SSH_USER}/.ssh \\
   --bind /home/${SSH_USER}/dev/asteroids:/home/${SSH_USER}/asteroids \\
   --bind ${TMP_DIR}:/tmp \\
